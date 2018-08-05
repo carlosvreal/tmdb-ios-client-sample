21fc08aa2c23f76c5837e2e07a2776fdf83a30cb
Carlos Real <cviniciusreal@gmail.com>
2018-08-05 08:55:09 -0300

    Fixing Settings class missing parser entity to get the app key from plist

--- MobileDB/Common/Settings.swift
+++ MobileDB/Common/Settings.swift
@@ -8,9 +8,18 @@
 
 import Foundation
 
+// Loads appKey tmdb from AppSettings.plist
+private struct AppSettings: Codable {
+  private enum CodingKeys: String, CodingKey {
+    case apiKey = "api_key"
+  }
+  
+  let apiKey: String
+}
+
 struct Settings {
   static var baseImageUrl = "http://image.tmdb.org/t/p/"
-
+  
   static var apiKey: String {
     guard let url = Bundle.main.url(forResource: "AppSettings", withExtension: "plist"),
       let data = try? Data(contentsOf: url),

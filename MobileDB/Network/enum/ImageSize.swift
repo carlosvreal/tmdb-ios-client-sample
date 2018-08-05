0ae7850fc66e006010ad147619d879f29aa9c206
Carlos Real <cviniciusreal@gmail.com>
2018-08-05 11:05:25 -0300

    Helper class to identify the best image size to use

---
+++ MobileDB/Network/enum/ImageSize.swift
@@ -0,0 +1,62 @@
+//
+//  ImageSize.swift
+//  MobileDB
+//
+//  Created by Carlos Vinicius on 8/05/18.
+//  Copyright © 2018 ArcTouch. All rights reserved.
+//
+
+import UIKit
+
+enum PosterSize: String {
+  case w92
+  case w154
+  case w185
+  case w342
+  case w500
+  case w780
+  case original
+}
+
+struct PosterImage {
+  static func bestSize() -> PosterSize {
+    switch UIDevice.current.userInterfaceIdiom {
+    case .phone:
+      if Device.isIPhone8PlusOrX {
+        return .w780
+      } else if Device.isIPhoneSE {
+        return .w342
+      }
+      
+      return .w500
+    case .pad:
+      return .w780
+    default:
+      return .original
+    }
+  }
+}
+
+enum BackdropSize: String {
+  case w300
+  case w780
+  case w1280
+  case original
+}
+
+struct BackdropImage {
+  static func bestSize() -> BackdropSize {
+    switch UIDevice.current.userInterfaceIdiom {
+    case .phone:
+      if Device.isIPhone8PlusOrX {
+        return .w780
+      }
+      
+      return .w300
+    case .pad:
+      return .w1280
+    default:
+      return .original
+    }
+  }
+}

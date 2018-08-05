0b39526376064cba30d95ba21697e6450f5625cb
Carlos Real <cviniciusreal@gmail.com>
2018-08-05 08:19:01 -0300

    ServiceProviderProtocol provides an abstraction to inject the implementation of SessionHandler

---
+++ MobileDB/Network/Protocols/ServiceProviderProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ServiceProviderProtocol.swift
+//  MobileDB
+//
+//  Created by Carlos Vinicius on 8/04/18.
+//  Copyright © 2018 ArcTouch. All rights reserved.
+//
+
+import RxSwift
+
+protocol ServiceProviderProtocol {
+  var session: SessionHandler { get }
+  
+  init(session: SessionHandler)
+}

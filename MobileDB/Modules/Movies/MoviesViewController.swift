09a4315d508966a0372f392a5eddbd6e925cd29a
Carlos Real <cviniciusreal@gmail.com>
2018-08-05 16:53:31 -0300

    Add Movies list modules files

---
+++ MobileDB/Modules/Movies/MoviesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesViewController.swift
+//  MobileDB
+//
+//  Created by Carlos Vinicius on 8/05/18.
+//  Copyright © 2018 ArcTouch. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ MobileDB/Modules/Movies/MoviesViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesViewModel.swift
+//  MobileDB
+//
+//  Created by Carlos Vinicius on 8/05/18.
+//  Copyright © 2018 ArcTouch. All rights reserved.
+//
+
+import Foundation
+
+struct MoviesViewModel {
+  
+}

bef8aaac1ad081d8984073f290eb417f919ddbbd
Carlos Real <cviniciusreal@gmail.com>
2018-08-06 01:35:42 -0300

    Adds logice to ViewModel to load data and pagination
    Parser genreIds to [Genre]

--- MobileDB/Modules/Movies/MoviesViewController.swift
+++ MobileDB/Modules/Movies/MoviesViewController.swift
@@ -7,10 +7,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxDataSources
 
-class MoviesViewController: UIViewController {
+final class MoviesViewController: UIViewController {
+  @IBOutlet private weak var tableView: UITableView!
+  
+  private let disposeBag = DisposeBag()
+  private let viewModel = MoviesViewModel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    tableView.register(MovieViewCell.self)
+    tableView.rx.setDelegate(self).disposed(by: disposeBag)
+    
+    setupOutletsBinds()
+  }
+  
+  private func setupOutletsBinds() {
+    viewModel.moviesDataSource.asObservable()
+      .bind(to: tableView.rx.items(cellIdentifier: MovieViewCell.identifier,
+                                   cellType: MovieViewCell.self)) {(_, movieViewModel, cell) in
+        cell.viewModel.setupData(with: movieViewModel)
+      }.disposed(by: disposeBag)
+    
+    // Triggers next page
+    tableView.rx.willDisplayCell
+      .filter { (_, indexPath) -> Bool in
+        return indexPath.item == self.tableView.numberOfRows(inSection: 0) - 3
+      }.map { _ in }
+      .subscribe(onNext: { [weak self] _ in
+        self?.viewModel.nextPage()
+      }).disposed(by: disposeBag)
+  }
+}
+
+extension MoviesViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    guard let cell = cell as? MovieViewCell else {
+      preconditionFailure("Invalid cell type")
+    }
+    
+    cell.viewModel.loadImage()
   }
 }
--- MobileDB/Modules/Movies/MoviesViewModel.swift
+++ MobileDB/Modules/Movies/MoviesViewModel.swift
@@ -6,8 +6,61 @@
 //  Copyright © 2018 ArcTouch. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
 
-struct MoviesViewModel {
+final class MoviesViewModel {
+  private let dataSource = Variable<[MovieViewModel]>([])
+  private var page = 1
+  private var cachedGenres = Variable<[Genre]>([])
+  let errorMessage = PublishSubject<String>()
+  let isLoading = PublishSubject<Bool>()
   
+  var moviesDataSource: Driver<[MovieViewModel]> {
+    return dataSource.asDriver()
+  }
+  
+  private let service: MoviesServiceProtocol
+  private let disposeBag: DisposeBag
+  
+  init(service: MoviesServiceProtocol = MoviesServiceProvider()) {
+    self.service = service
+    self.disposeBag = DisposeBag()
+    
+    nextPage()
+  }
+  
+  func nextPage() {
+    isLoading.onNext(true)
+    
+    let fetchMovies = service.fetchMovies(from: page)
+    let genres = service.genres()
+      .do(onSuccess: { [weak self] genres in
+        self?.cachedGenres.value.append(contentsOf: genres)
+      })
+    
+    Single.zip(fetchMovies, genres) { ($0, $1) }
+      .map { (movies, genres) -> [MovieViewModel] in
+        return movies.map { movie -> MovieViewModel in
+          let movieGenres = movie.genreIds?.map { id -> Genre? in
+            return genres.first(where: { $0.id == id })
+            }.compactMap { $0 }
+          
+          return MovieViewModel(title: movie.title,
+                                imagePath: movie.poster,
+                                ratingScore: movie.rating,
+                                releaseYear: movie.releaseDate,
+                                genres: movieGenres)
+        }
+      }
+      .observeOn(MainScheduler.asyncInstance)
+      .subscribe(onSuccess: { [unowned self] movies in
+        self.dataSource.value.append(contentsOf: movies)
+        self.page += 1
+        self.isLoading.onNext(false)
+      }, onError: { [unowned self] error in
+        self.isLoading.onNext(false)
+        self.errorMessage.onNext(error.localizedDescription)
+      }).disposed(by: disposeBag)
+  }
 }

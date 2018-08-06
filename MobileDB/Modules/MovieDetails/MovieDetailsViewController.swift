a0195c12ac7a98c82c52319df48198163980e787
Carlos Real <cviniciusreal@gmail.com>
2018-08-06 17:54:22 -0300

    Adds logic to form MovieDetail content
    Includes new placeholder image asset

--- MobileDB/Modules/MovieDetails/MovieDetailsViewController.swift
+++ MobileDB/Modules/MovieDetails/MovieDetailsViewController.swift
@@ -6,11 +6,39 @@
 //  Copyright © 2018 ArcTouch. All rights reserved.
 //
 
-import UIKit
+import RxSwift
+import RxCocoa
 
 class MovieDetailsViewController: UIViewController {
+  @IBOutlet private weak var backdropImageView: UIImageView!
+  @IBOutlet private weak var titleMovie: UILabel!
+  @IBOutlet private weak var releaseYear: UILabel!
+  @IBOutlet private weak var runtime: UILabel!
+  @IBOutlet private weak var language: UILabel!
+  @IBOutlet private weak var ratingScore: UILabel!
+  @IBOutlet private weak var descriptionMovie: UILabel!
+  @IBOutlet private weak var genres: UILabel!
+  @IBOutlet private weak var revenue: UILabel!
+  
+  var viewModel: MovieDetailsViewModel?
+  private let disposeBag = DisposeBag()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    setupUIBind()
+  }
+  
+  private func setupUIBind() {
+    viewModel?.backdropImage.bind(to: backdropImageView.rx.image).disposed(by: disposeBag)
+    viewModel?.titleMovie.bind(to: titleMovie.rx.text).disposed(by: disposeBag)
+    viewModel?.releaseYear.bind(to: releaseYear.rx.text).disposed(by: disposeBag)
+    viewModel?.runtime.bind(to: runtime.rx.text).disposed(by: disposeBag)
+    viewModel?.language.bind(to: language.rx.text).disposed(by: disposeBag)
+    viewModel?.descriptionMovie.bind(to: descriptionMovie.rx.text).disposed(by: disposeBag)
+    viewModel?.genres.bind(to: genres.rx.text).disposed(by: disposeBag)
+    viewModel?.revenue.bind(to: revenue.rx.text).disposed(by: disposeBag)
+    
+    viewModel?.setupData()
   }
 }
--- MobileDB/Modules/MovieDetails/MovieDetailsViewModel.swift
+++ MobileDB/Modules/MovieDetails/MovieDetailsViewModel.swift
@@ -6,7 +6,102 @@
 //  Copyright © 2018 ArcTouch. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
 
-struct MovieDetailsViewModel {
+final class MovieDetailsViewModel {
+  let backdropImage = PublishSubject<UIImage>()
+  let titleMovie = PublishSubject<String>()
+  let releaseYear = PublishSubject<String>()
+  let runtime = PublishSubject<String>()
+  let language = PublishSubject<String>()
+  let ratingScore = PublishSubject<String>()
+  let descriptionMovie = PublishSubject<String>()
+  let genres = PublishSubject<String>()
+  let revenue = PublishSubject<String>()
+
+  private let model: MovieDetailModel
+  private let service: ConfigServiceProtocol
+  private let disposeBag = DisposeBag()
+  
+  init(model: MovieDetailModel, service: ConfigServiceProtocol = ConfigServiceProvider()) {
+    self.model = model
+    self.service = service
+  }
+
+  func setupData() {
+    if let title = model.title {
+      titleMovie.onNext(title)
+    }
+    
+    if let releaseYear = model.releaseYear,
+      let year = releaseYear.split(separator: "-").first {
+      self.releaseYear.onNext(String(year))
+    }
+    
+    if let ratingScore = model.ratingScore {
+      let ratingFormatted = String(format: "%.1f", ratingScore)
+      self.ratingScore.onNext(ratingFormatted)
+    }
+    
+    if let genres = model.genres {
+      self.genres.onNext(genres.formatGenresAsString())
+    }
+    
+    if let runtime = model.runtime {
+      let formattedRuntime = formatTime(Double(runtime))
+      self.runtime.onNext(formattedRuntime)
+    }
+    
+    if let language = model.language {
+      self.language.onNext(language)
+    }
+    
+    if let description = model.description {
+      self.descriptionMovie.onNext(description)
+    }
+    
+    if let revenue = model.revenue,
+      let formattedRevenue = formatToCurrency(revenue) {
+      self.revenue.onNext(formattedRevenue)
+    }
+    
+    setupLoadBackdropImage()
+  }
+}
+
+// MARK: Utility methods
+private extension MovieDetailsViewModel {
+  func setupLoadBackdropImage() {
+    guard let backdrop = model.backdropImagePath else { return }
+    _ = service
+      .loadBackdropImage(for: backdrop)
+      .observeOn(MainScheduler.asyncInstance)
+      .subscribe(onSuccess: { [weak self] image in
+        self?.backdropImage.onNext(image)
+      })
+  }
+  
+  func formatTime(_ time: Double) -> String {
+    guard time > 3600 else {
+      return "\(Int(time / 60))m"
+    }
+    
+    let totalHours = time / 3600
+    let minutes = totalHours.truncatingRemainder(dividingBy: 1) * 60
+    return "\(Int(totalHours))h \(Int(minutes))m"
+  }
+  
+  func formatToCurrency(_ amount: Int) -> String? {
+    let formatter = NumberFormatter()
+    formatter.usesGroupingSeparator = true
+    formatter.numberStyle = .currency
+    formatter.locale = Locale.current
+    
+    guard let formattedValue = formatter.string(from: NSNumber(value: amount)) else {
+      return nil
+    }
+
+    return formattedValue
+  }
 }

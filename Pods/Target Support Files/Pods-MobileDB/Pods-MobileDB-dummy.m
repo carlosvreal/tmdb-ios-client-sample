ca3610ffae414263ee8d5049ff20f23add94c7bc
Carlos Real <cviniciusreal@gmail.com>
2020-08-20 08:37:56 -0300

    Removing cocoa pods folder

--- Pods/Target Support Files/Pods-MobileDB/Pods-MobileDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileDB : NSObject
-@end
-@implementation PodsDummy_Pods_MobileDB
-@end
--- Pods/Target Support Files/Pods-MobileDBTests/Pods-MobileDBTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileDBTests : NSObject
-@end
-@implementation PodsDummy_Pods_MobileDBTests
-@end
